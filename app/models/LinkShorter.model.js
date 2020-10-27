37daff2209f33eca29c2fbb311cdd107b83d52cd
Dmytro <dimaniko04@ukr.net>
2020-10-27 18:31:53 +0200

    It works

--- app/models/LinkShorter.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const LinkShorterSchema = new mongoose.Schema({ code: String });
-
-const LinkShorter = mongoose.model("LinkShorter", LinkShorterSchema);
-
-module.exports = LinkShorter;

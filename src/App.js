d70ad2d63cbb1be90b6b2d27be9d6347e0226fae
Dmytro <dimaniko04@ukr.net>
2020-10-27 18:16:13 +0200

    It works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 function App() {
+  const [UserInput, SetUserInput] = useState({});
+  const [AllLinks, setAllLinks] = useState(<></>);
+
+  const onLinkChange = (e) => {
+    SetUserInput({ Link: e.target.value });
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/")
+      .then((res) => {
+        if (res.data.length > 0) {
+          setAllLinks(
+            res.data.map((Data) => {
+              return (
+                <div className="link">
+                  <a href={Data.FullLink}>{Data.FullLink}</a>
+                  <br />
+                  <a href={Data.FullLink}>
+                    {"http://localhost:5000/" + Data.ShortenLink}
+                  </a>
+                </div>
+              );
+            })
+          );
+        }
+      })
+      .catch((err) => console.log(err));
+  });
+
+  const ShorterLink = () => {
+    axios
+      .post("http://localhost:5000/add", UserInput)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Link Shorter">
+        <input
+          onChange={(e) => onLinkChange(e)}
+          placeholder="Write the link here"
+        />
+        <button onClick={() => ShorterLink()}>Shorter</button>
+      </div>
+      <div className="Links">{AllLinks}</div>
+    </>
   );
 }
 
